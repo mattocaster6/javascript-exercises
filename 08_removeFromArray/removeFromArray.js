93748c783a2e69f653adfb03e308c227880f86ff
mattocaster6 <35231964+mattocaster6@users.noreply.github.com>
2025-07-04 18:04:55 +0100

    Complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    for(const item of args) {
+
+        while(arr.includes(item)) {
+            arr.splice(arr.findIndex(obj => obj === item), 1);
+        }
+
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
