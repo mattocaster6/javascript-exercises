a02193732be6341d618b711740c929e02537e4c5
mattocaster6 <35231964+mattocaster6@users.noreply.github.com>
2025-07-05 19:40:21 +0100

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    const reversedString = cleanString.split("").toReversed().join("");
+
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
