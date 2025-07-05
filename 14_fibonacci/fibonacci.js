3830749037ef19f0d0e706b2f5c18e9fb40d1ae0
mattocaster6 <35231964+mattocaster6@users.noreply.github.com>
2025-07-05 20:13:44 +0100

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+
+    let a = 0, b = 1;
+
+    for (let i = 1; i < n; i++) {
+        [a, b] = [b, a + b];
+    }
+
+    return b;
 };
 
 // Do not edit below this line
