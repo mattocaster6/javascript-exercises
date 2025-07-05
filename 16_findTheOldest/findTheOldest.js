63e5cc3c44cfba080af23c5c03e06a23c88b71df
mattocaster6 <35231964+mattocaster6@users.noreply.github.com>
2025-07-05 20:39:24 +0100

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    function getAge(person) {
+        return person.yearOfDeath === undefined ? new Date().getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+    }
+
+    return people.reduce((oldest, person) => {
+
+        const personAge = getAge(person);
+        const oldestAge = getAge(oldest);
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
